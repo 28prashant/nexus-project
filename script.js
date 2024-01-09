c5d43e8ed11df061a0b92280ea6f4f0f1c3d0f22
Prashant Kumar <kumarprashant00505@gmail.com>
2024-01-10 01:03:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    // Implement login form validation and handling here
+    // Display error messages using document.getElementById('loginErrorMessage').textContent
+    event.preventDefault();
+  });
+
+  document.getElementById('signupForm').addEventListener('submit', function(event) {
+    // Implement signup form validation and handling here
+    // Display error messages using document.getElementById('signupErrorMessage').textContent
+    event.preventDefault();
+  });
